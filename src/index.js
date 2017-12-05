7d2c883f1ed8d7dbd5d715f8798b37610ff32e4c
Laurent Michel <laurent1984@gmail.com>
2017-12-05 22:00:15 +0100

    started wrapping config with webpack

---
+++ src/index.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+  $("#content").load('blog.html');
+});
+
+/*var AccordionLoader = {
+  resetPanel: function (panel) {
+    panel.style.maxHeight = null;
+    panel.style.borderWidth = null;
+    panel.style.borderColor = "white";
+  },
+  deactivateAll: function (param) {
+    var accs = document.getElementsByClassName("accordion");
+    for (var i = 0; i < accs.length; ++i) {
+      if (accs[i] == param) {
+        continue;
+      }
+      var panel = accs[i].nextElementSibling;
+      if (panel.style.maxHeight) {
+        accs[i].classList.toggle("active");
+        AccordionLoader.resetPanel(panel);
+      }
+    }
+  },
+  toggle: function (param) {
+    AccordionLoader.deactivateAll(param);
+    param.classList.toggle("active");
+    var panel = param.nextElementSibling;
+    if (panel.style.maxHeight) {
+      AccordionLoader.resetPanel(panel);
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      //panel.style.borderColor = getComputedStyle(document.body).getPropertyValue('--principalColor');
+      panel.style.borderStyle = "solid";
+      panel.style.borderWidth = "2px";
+    }
+  },
+  showTeam: function (lang) {
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // code for IE6, IE5
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("teamContent").innerHTML = this.responseText;
+      }
+    };
+
+    if (lang.empty) {
+      lang = "en";
+    }
+
+    xmlhttp.open("GET", "scripts/getTeam.php?lang=" + lang, true);
+    xmlhttp.send();
+  }
+};
+
+var ContactLoader = {
+  showForm: function (lang, elementId) {
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // code for IE6, IE5
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById(elementId).innerHTML = this.responseText;
+      }
+    };
+
+    if (lang.empty) {
+      lang = "en";
+    }
+
+    xmlhttp.open("GET", "scripts/getContactForm.php?&lang=" + lang, true);
+    xmlhttp.send();
+  }
+};
+
+var ContentLoader = {
+  getText: function (contentId, lang, elementId) {
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // code for IE6, IE5
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById(elementId).innerHTML = this.responseText;
+      }
+    };
+
+    if (lang.empty) {
+      lang = "en";
+    }
+
+    xmlhttp.open("GET", "scripts/getContent.php?contentId=" + contentId + "&lang=" + lang, true);
+    xmlhttp.send();
+  }
+};
+
+var MainNavigation = {
+  load: function (file) {
+    $("#content").load(file);
+  }
+};
+*/
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  }, 
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+  })]
+};
